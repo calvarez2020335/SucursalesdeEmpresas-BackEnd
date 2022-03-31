598067fadad0e20ffcc64db6e3b7447eaca724df
Carlos Adolfo Alvarez Crúz <calvarez-2020335@kinal.edu.gt>
2022-03-31 15:57:13 -0600

    controller y  models

---
+++ src/models/empleados.model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const EmpleadosSchema = Schema({
+    nombre: String,
+    apellido: String,
+    puesto: String,
+    departamento: String,
+    idEmpresa: {type: Schema.Types.ObjectId, ref: 'Usuarios'}
+});
+
+module.exports = mongoose.model('Empleados', EmpleadosSchema);
