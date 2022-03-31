a61255ef7e5f3af4509525b67646df6d5ff91abf
Carlos Adolfo Alvarez Crúz <calvarez-2020335@kinal.edu.gt>
2022-03-31 15:41:20 -0600

    Carlos
    autenticación, jwt y roles

---
+++ src/middlewares/roles.js
@@ -0,0 +1,9 @@
+exports.verEmpresas = function(req, res, next) {
+    if(req.user.rol !== "ROL_EMPRESA") return res.status(403).send({mensaje: "Solo puede acceder la empresa"})
+    next();
+}
+
+exports.verAdministrador = function(req, res, next) {
+    if(req.user.rol !== "ROL_ADMIN") return res.status(403).send({mensaje: "Solo puede acceder el ADMIN"})
+    next();
+}
