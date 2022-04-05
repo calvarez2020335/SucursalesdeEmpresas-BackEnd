956d68afa42e2cc2f27f42f6957e649f2b3c4803
Informatica Kinal <88805710+adelacruz2020390@users.noreply.github.com>
2022-04-05 16:55:11 -0600

    push

--- src/routes/sucursales.routes.js
+++ src/routes/sucursales.routes.js
@@ -8,6 +8,7 @@ const md_roles = require('../middlewares/roles');
 const api = express.Router();
 
 api.post('/agregarSucursales', [md_autenticacion.Auth, md_roles.verEmpresas], controladorSurcusales.agregarSucursales);
+api.put('/editarSurcursal/:idSurcursal', [md_autenticacion.Auth, md_roles.verEmpresas] ,controladorSurcusales.editarSurcursal)
 //api.get('/Empleados', controladorEmpleados.buscarTodosLosEmpleados)
 
 module.exports = api;
