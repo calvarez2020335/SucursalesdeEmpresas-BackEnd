7f6e82a15d3104aa8bacedf0620dc49985941a80
Informatica Kinal <88805710+adelacruz2020390@users.noreply.github.com>
2022-04-05 15:40:00 -0600

    a

--- src/models/sucursales.model.js
+++ src/models/sucursales.model.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const SucursalesSchema = Schema({
+const EmpleadosSchema = Schema({
     nombre: String,
-    telefono: String,
-    direccion: String,
-    stock: Number,
-    vendido:Number,
+    apellido: String,
+    puesto: String,
+    departamento: String,
     idEmpresa: {type: Schema.Types.ObjectId, ref: 'Usuarios'}
+    
 });
 
-module.exports = mongoose.model('Sucursales', SucursalesSchema);
+module.exports = mongoose.model('Empleados', EmpleadosSchema);
