fad79c6023ec0358d3b9d6d04d4317824f6ebd3f
Carlos Adolfo Alvarez Crúz <calvarez-2020335@kinal.edu.gt>
2022-05-01 07:02:54 -0600

    Actualizacion

--- src/controllers/sucursales.controller.js
+++ src/controllers/sucursales.controller.js
@@ -6,7 +6,11 @@ function agregarSucursales(req, res) {
   const parametros = req.body;
   const modeloSucursales = new Sucursales();
 
-  if (parametros.nombre && parametros.telefono && parametros.direccion) {
+  if (
+    parametros.nombre &&
+    parametros.telefono &&
+    parametros.direccion
+  ) {
     modeloSucursales.nombre = parametros.nombre;
     modeloSucursales.telefono = parametros.telefono;
     modeloSucursales.direccion = parametros.direccion;
@@ -101,23 +105,21 @@ function editarSurcursal(req, res) {
 //Buscar todas las sucursales
 
 function verSucursalesEmpresas(req, res) {
-  Sucursales.find(
-    { idEmpresa: req.user.sub },
-    (err, sucursalEmpresaEncontrada) => {
-      return res.status(200).send({ Sucursales: sucursalEmpresaEncontrada });
-    }
-  );
+
+  Sucursales.find({ idEmpresa: req.user.sub }, (err, sucursalEmpresaEncontrada) => {
+    return res.status(200).send({ Sucursales: sucursalEmpresaEncontrada })
+  })
+
 }
 
 //Se usa para poder editar la sucursal
 function verSucursalesId(req, res) {
+
   const idSucursal = req.params.idSucursal;
-  Sucursales.findById(
-    { _id: idSucursal, idEmpresa: req.user.sub },
-    (err, sucursalId) => {
-      return res.status(200).send({ Sucursal: sucursalId });
-    }
-  );
+  Sucursales.findById({ _id: idSucursal, idEmpresa: req.user.sub }, (err, sucursalId) => {
+    return res.status(200).send({ Sucursal: sucursalId });
+  })
+
 }
 
 //Produtos Por Sucursales ---------------------------------------------------------------------
@@ -203,133 +205,122 @@ function agregarProductosSurcursales(req, res) {
 function VerProductosPorSucursales(req, res) {
   var idSurcursal = req.params.idSurcursal;
   ProductoSurcursales.find(
-    { idSurcursal: idSurcursal, idEmpresa: req.user.sub },
+    {idSurcursal: idSurcursal ,idEmpresa: req.user.sub },
     (err, productoEncontrado) => {
       if (err)
         return res.status(404).send({ mensaje: "Producto no encontrado" });
-      if (!productoEncontrado)
-        return res.status(404).send({ mensaje: "Productossssss No hallados" });
+        if(!productoEncontrado) return res.status(404).send({ mensaje: "Productossssss No hallados"})
+      return res.status(200).send({ Productos: productoEncontrado });
+    }
+  )
+}
+
+function VentaSimuladaSurcursales(req, res) {
+  const parametros = req.body;
+  const idSurcu = req.params.idSurcursal
+
+  if (
+    parametros.NombreProductoSurcur && parametros.StockVender
+  ) {
+
+    Sucursales.findOne({ _id: idSurcu, idEmpresa: req.user.sub }, (err, sucursalEmpresaEncontrada) => {
+      if (!sucursalEmpresaEncontrada) return res.status(404).send({ mensaje: "surcursal no encontrada" });
+      if (err) return res.status(404).send({ mensaje: "surcursal no encontrada" });
+
+      ProductoSurcursales.findOne({ NombreProductoSucursal: parametros.NombreProductoSurcur, idSurcursal: sucursalEmpresaEncontrada.id }, (err, ProductoSurcursalesEncontrada) => {
+        if (err) return res.status(404).send({ mensaje: "producto no encontrada surcursales" });
+
+        if (parametros.StockVender <= 0) {
+          return res.status(404).send({ mensaje: "formato incorrecto" });
+        }
+
+        if (parametros.StockVender > ProductoSurcursalesEncontrada.StockSurcursal) {
+          return res.status(404).send({ mensaje: "no hay stock " });
+        }
+
+        const data = {
+          StockSurcursal: ProductoSurcursalesEncontrada.StockSurcursal,
+          CantidadVendida: ProductoSurcursalesEncontrada.CantidadVendida
+        }
+        data.StockSurcursal = ProductoSurcursalesEncontrada.StockSurcursal - parametros.StockVender
+        data.CantidadVendida = parseFloat(data.CantidadVendida)  + parseFloat(parametros.StockVender) 
+
+        if (ProductoSurcursalesEncontrada == null) {
+          return res.status(404).send({ mensaje: "producto no encontrada en surcursales" });
+
+        } else {
+
+          ProductoSurcursales.findByIdAndUpdate({ _id: ProductoSurcursalesEncontrada.id }, data, { new: true }, (err, StockModificado) => {
+            if (!StockModificado) return res.status(404).send({ mensaje: "Producto no encontrado" });
+            if (err) return res.status(404).send({ mensaje: "Producto no encontrado" });
+
+            return res.status(404).send({ productoafectado: StockModificado });
+          })
+        }
+      })
+
+    })
+
+  } else {
+    return res.status(500).send({ Surcusal: "enviar parametros obligatorios" });
+  }
+
+}
+
+function OrdenarStockSurcursaleskMayor(req, res) {
+  const idSurcu = req.params.idSurcursal
+
+  ProductoSurcursales.find(
+    { idSurcursal: idSurcu, idEmpresa: req.user.sub },
+    (err, productoEncontrado) => {
+      if (err)
+        return res.status(404).send({ mensaje: "Producto no encontrado" });
+      return res.status(200).send({ Productos: productoEncontrado });
+    }
+  ).sort({StockSurcursal: -1})
+
+}
+
+function OrdenarStockSurcursaleskMenor(req, res) {
+  const idSurcu = req.params.idSurcursal
+  ProductoSurcursales.find(
+    { idSurcursal: idSurcu, idEmpresa: req.user.sub },
+    (err, productoEncontrado) => {
+      if (err)
+        return res.status(404).send({ mensaje: "Producto no encontrado" });
+      return res.status(200).send({ Productos: productoEncontrado });
+    }
+  ).sort({StockSurcursal: 1})
+
+}
+
+function ElMasVendidoProductos(req, res) {
+  const idSurcu = req.params.idSurcursal
+  ProductoSurcursales.find(
+    {idSurcursal: idSurcu, idEmpresa: req.user.sub },
+    (err, productoEncontrado) => {
+      if (err)
+        return res.status(404).send({ mensaje: "Producto no encontrado" });
+      return res.status(200).send({ Productos: productoEncontrado });
+    }
+  ).sort({CantidadVendida: -1})
+
+}
+
+function VerProductosSurucrsalesId(req, res) {
+  const idProducto = req.params.idProducto;
+
+  ProductoSurcursales.findById(
+    { _id: idProducto},
+    (err, productoEncontrado) => {
+      if (err)
+        return res.status(404).send({ mensaje: "Producto no encontrado" });
       return res.status(200).send({ Productos: productoEncontrado });
     }
   );
 }
 
-function VentaSimuladaSurcursales(req, res) {
-  const parametros = req.body;
-  const idSurcu = req.params.idSurcursal;
 
-  if (parametros.NombreProductoSurcur && parametros.StockVender) {
-    Sucursales.findOne(
-      { _id: idSurcu, idEmpresa: req.user.sub },
-      (err, sucursalEmpresaEncontrada) => {
-        if (!sucursalEmpresaEncontrada)
-          return res.status(404).send({ mensaje: "surcursal no encontrada" });
-        if (err)
-          return res.status(404).send({ mensaje: "surcursal no encontrada" });
-
-        ProductoSurcursales.findOne(
-          {
-            NombreProductoSucursal: parametros.NombreProductoSurcur,
-            idSurcursal: sucursalEmpresaEncontrada.id,
-          },
-          (err, ProductoSurcursalesEncontrada) => {
-            if (err)
-              return res
-                .status(404)
-                .send({ mensaje: "producto no encontrada surcursales" });
-
-            if (parametros.StockVender <= 0) {
-              return res.status(404).send({ mensaje: "formato incorrecto" });
-            }
-
-            if (
-              parametros.StockVender >
-              ProductoSurcursalesEncontrada.StockSurcursal
-            ) {
-              return res.status(404).send({ mensaje: "no hay stock " });
-            }
-
-            const data = {
-              StockSurcursal: ProductoSurcursalesEncontrada.StockSurcursal,
-              CantidadVendida: ProductoSurcursalesEncontrada.CantidadVendida,
-            };
-            data.StockSurcursal =
-              ProductoSurcursalesEncontrada.StockSurcursal -
-              parametros.StockVender;
-            data.CantidadVendida =
-              parseFloat(data.CantidadVendida) +
-              parseFloat(parametros.StockVender);
-
-            if (ProductoSurcursalesEncontrada == null) {
-              return res
-                .status(404)
-                .send({ mensaje: "producto no encontrada en surcursales" });
-            } else {
-              ProductoSurcursales.findByIdAndUpdate(
-                { _id: ProductoSurcursalesEncontrada.id },
-                data,
-                { new: true },
-                (err, StockModificado) => {
-                  if (!StockModificado)
-                    return res
-                      .status(404)
-                      .send({ mensaje: "Producto no encontrado" });
-                  if (err)
-                    return res
-                      .status(404)
-                      .send({ mensaje: "Producto no encontrado" });
-
-                  return res
-                    .status(404)
-                    .send({ productoafectado: StockModificado });
-                }
-              );
-            }
-          }
-        );
-      }
-    );
-  } else {
-    return res.status(500).send({ Surcusal: "enviar parametros obligatorios" });
-  }
-}
-
-function OrdenarStockSurcursaleskMayor(req, res) {
-  const idSurcu = req.params.idSurcursal;
-
-  ProductoSurcursales.find(
-    { idSurcursal: idSurcu, idEmpresa: req.user.sub },
-    (err, productoEncontrado) => {
-      if (err)
-        return res.status(404).send({ mensaje: "Producto no encontrado" });
-      return res.status(200).send({ Productos: productoEncontrado });
-    }
-  ).sort({ StockSurcursal: -1 });
-}
-
-function OrdenarStockSurcursaleskMenor(req, res) {
-  const idSurcu = req.params.idSurcursal;
-  ProductoSurcursales.find(
-    { idSurcursal: idSurcu, idEmpresa: req.user.sub },
-    (err, productoEncontrado) => {
-      if (err)
-        return res.status(404).send({ mensaje: "Producto no encontrado" });
-      return res.status(200).send({ Productos: productoEncontrado });
-    }
-  ).sort({ StockSurcursal: 1 });
-}
-
-function ElMasVendidoProductos(req, res) {
-  const idSurcu = req.params.idSurcursal;
-  ProductoSurcursales.find(
-    { idSurcursal: idSurcu, idEmpresa: req.user.sub },
-    (err, productoEncontrado) => {
-      if (err)
-        return res.status(404).send({ mensaje: "Producto no encontrado" });
-      return res.status(200).send({ Productos: productoEncontrado });
-    }
-  ).sort({ CantidadVendida: -1 });
-}
 module.exports = {
   agregarSucursales,
   eliminarSucursales,
@@ -340,7 +331,10 @@ module.exports = {
   agregarProductosSurcursales,
   VerProductosPorSucursales,
   VentaSimuladaSurcursales,
-  OrdenarStockSurcursaleskMayor,
+  OrdenarStockSurcursaleskMayor ,
   ElMasVendidoProductos,
   OrdenarStockSurcursaleskMenor,
+  VerProductosSurucrsalesId
+
+
 };
