9050b37f5cccb8f87f73273d1975cc8f6bc3f3aa
mherrera 2020235 <88869102+MarckHerrera@users.noreply.github.com>
2022-05-03 12:49:22 -0600

    Marck
    cambios en Venta

--- src/controllers/sucursales.controller.js
+++ src/controllers/sucursales.controller.js
@@ -220,21 +220,21 @@ function VentaSimuladaSurcursales(req, res) {
   const idSurcu = req.params.idSurcursal
 
   if (
-    parametros.NombreProductoSurcur && parametros.StockVender
+    parametros.NombreProductoSucursal && parametros.StockSurcursal
   ) {
 
     Sucursales.findOne({ _id: idSurcu, idEmpresa: req.user.sub }, (err, sucursalEmpresaEncontrada) => {
       if (!sucursalEmpresaEncontrada) return res.status(404).send({ mensaje: "surcursal no encontrada" });
       if (err) return res.status(404).send({ mensaje: "surcursal no encontrada" });
 
-      ProductoSurcursales.findOne({ NombreProductoSucursal: parametros.NombreProductoSurcur, idSurcursal: sucursalEmpresaEncontrada.id }, (err, ProductoSurcursalesEncontrada) => {
+      ProductoSurcursales.findOne({ NombreProductoSucursal: parametros.NombreProductoSucursal, idSurcursal: sucursalEmpresaEncontrada.id }, (err, ProductoSurcursalesEncontrada) => {
         if (err) return res.status(404).send({ mensaje: "producto no encontrada surcursales" });
 
-        if (parametros.StockVender <= 0) {
+        if (parametros.StockSurcursal <= 0) {
           return res.status(404).send({ mensaje: "formato incorrecto" });
         }
 
-        if (parametros.StockVender > ProductoSurcursalesEncontrada.StockSurcursal) {
+        if (parametros.StockSurcursal > ProductoSurcursalesEncontrada.StockSurcursal) {
           return res.status(404).send({ mensaje: "no hay stock " });
         }
 
@@ -242,8 +242,8 @@ function VentaSimuladaSurcursales(req, res) {
           StockSurcursal: ProductoSurcursalesEncontrada.StockSurcursal,
           CantidadVendida: ProductoSurcursalesEncontrada.CantidadVendida
         }
-        data.StockSurcursal = ProductoSurcursalesEncontrada.StockSurcursal - parametros.StockVender
-        data.CantidadVendida = parseFloat(data.CantidadVendida)  + parseFloat(parametros.StockVender) 
+        data.StockSurcursal = ProductoSurcursalesEncontrada.StockSurcursal - parametros.StockSurcursal
+        data.CantidadVendida = parseFloat(data.CantidadVendida)  + parseFloat(parametros.StockSurcursal) 
 
         if (ProductoSurcursalesEncontrada == null) {
           return res.status(404).send({ mensaje: "producto no encontrada en surcursales" });
@@ -254,7 +254,7 @@ function VentaSimuladaSurcursales(req, res) {
             if (!StockModificado) return res.status(404).send({ mensaje: "Producto no encontrado" });
             if (err) return res.status(404).send({ mensaje: "Producto no encontrado" });
 
-            return res.status(404).send({ productoafectado: StockModificado });
+            return res.status(200).send({ productoafectado: StockModificado });
           })
         }
       })
