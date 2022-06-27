c90794dcc2cdba2ce9a70c19910053a7a1cacd5b
Carlos Adolfo Alvarez Crúz <calvarez-2020335@kinal.edu.gt>
2022-06-27 13:47:58 -0600

    Configuracio env

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const mongoose = require('mongoose');
 const app = require('./app');
 const {registrarAdmin} = require('./src/controllers/usuario.controller')
@@ -5,11 +6,11 @@ const {registrarAdmin} = require('./src/controllers/usuario.controller')
 mongoose.Promise = global.Promise;
 
 mongoose.Promise = global.Promise;                                                                  
-mongoose.connect('mongodb://localhost:27017/Empresas', { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
+mongoose.connect( process.env.DB_CONECTION, { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
     console.log("Conectado a la base de datos.");
 
-    app.listen(3000, function () {
-        console.log("Corriendo en el puerto 3000!")
+    app.listen(process.env.PORT || 3000, function () {
+        console.log("Corriendo en el puerto!")
     })
 
 }).catch(error => console.log(error));
